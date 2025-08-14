cc139d5c365b26de72764ace299428996fa77bda
youssef <youssef.magdy.office@gmail.com>
2025-08-14 19:52:52 +0200

    f

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -4,6 +4,12 @@ let quoteDisplay = document.getElementById("quoteDisplay");
 let newQuote = document.getElementById("newQuote");
 let quotes = [];
 
+function getFromLocalStorage() {
+    return JSON.parse(localStorage.getItem("quotes")) || [];
+}
+function saveToLocalStorage(quotes) {
+    localStorage.setItem("quotes", JSON.stringify(quotes));
+}
 function createAddQuoteForm() {
     let quotText = newQuoteText.value.trim();
     let quotCategory = newQuoteCategory.value.trim();
@@ -19,15 +25,36 @@ function createAddQuoteForm() {
     };
 
     quotes.push(quote);
-
-    let li = document.createElement("li");
-    li.innerHTML = `<strong>Quote:</strong> ${quote.text}<br><strong>Category:</strong> ${quote.category}`;
-    quoteDisplay.appendChild(li);
-
+    saveToLocalStorage(quotes);
     newQuoteText.value = "";
     newQuoteCategory.value = "";
+    renderQuotes();
 }
+function renderQuotes() {
+    quotes = getFromLocalStorage();
+    quoteDisplay.innerHTML = "";
 
+    if (quotes.length === 0) {
+        quoteDisplay.innerHTML = "<li>No quotes available.</li>";
+        return;
+    }
+
+    quotes.forEach((quote ,index) => {
+        let li = document.createElement("li");
+        li.innerHTML = `<strong>Quote:</strong> ${quote.text}<br><strong>Category:</strong> ${quote.category}`;
+        let deleteButton = document.createElement("button");
+        deleteButton.textContent = "Delete";
+        li.appendChild(deleteButton);
+        deleteButton.classList.add("delete-button");
+        deleteButton.addEventListener("click", () => {
+            quotes.splice(index, 1);
+            saveToLocalStorage(quotes);
+            renderQuotes();
+        });
+        quoteDisplay.appendChild(li);
+    });
+}
+renderQuotes() ;
 function showRandomQuote() {
     if (quotes.length === 0) {
         alert("No quotes available.");
